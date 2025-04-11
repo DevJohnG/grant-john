164c949fde8693159c2b2b2993c43ac298647d98
DevJohnG <john.grant@utp.ac.pa>
2025-04-10 23:18:01 -0500

    Cambios finales

--- diagnostica.js
+++ diagnostica.js
@@ -1,18 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
     const revealButton = document.getElementById('boton');
     const hiddenInfo = document.getElementById('masinfo');
+    const imageElement = document.getElementById('perfil');
 
     hiddenInfo.style.display = 'none';
 
     revealButton.addEventListener('click', () => {
-
         if (hiddenInfo.style.display === 'none' || hiddenInfo.style.display === '') {
             hiddenInfo.style.display = 'block';
             revealButton.textContent = 'MOSTRAR MENOS';
         } else {
             hiddenInfo.style.display = 'none';
             revealButton.textContent = 'MOSTRAR MÁS';
+        }
+    });
+
+    imageElement.addEventListener('click', () => {
+        if (imageElement.src.includes('FotoJohn.png')) { 
+            imageElement.src = 'FotoCarnet.jpg';
+        } else {
+            imageElement.src = 'FotoJohn.png';
         }
     });
 });
