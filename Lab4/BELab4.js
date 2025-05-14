f5562bdfab4013c4fa6baf81d6d91b571959ae36
DevJohnG <john.grant@utp.ac.pa>
2025-05-14 07:19:37 -0500

    Commit que no hice la semana pasada :/

--- Lab4/BELab4.js
+++ Lab4/BELab4.js
@@ -1,55 +1,79 @@
 (() => {
-    const App = {
-      htmlElements: {
-        formProduct : document.querySelector('#formProduct'),
-        productNumber : document.querySelector('#productNumber'),
-        productNumberOutput : document.querySelector('#productTableContainer')
-        
-      },
-      init() {
+  const App = {
+    htmlElements: {
+      formProduct: document.querySelector('#formProduct'),
+      productNumber: document.querySelector('#productNumber'),
+      productNumberOutput: document.querySelector('#productTableContainer'),
 
-        App.htmlElements.formProduct.addEventListener('submit', App.methods.generateProductTable);
+    },
+    init() {
 
-      },
-      methods: {
+      App.htmlElements.formProduct.addEventListener('submit', App.methods.numberValidation);
+      App.htmlElements.productNumberOutput.addEventListener('click', App.methods.deleteCurrentRow);
+    
+    },
+    methods: {
 
-        generateProductTable(e){
-            e.preventDefault();
-            const product = parseInt(e.target.elements.productNumber.value);
-            let i;  
-            let outPut = App.htmlElements.productNumberOutput;
+      generateProductTable(e) {
+        e.preventDefault();
+        const product = parseInt(e.target.elements.productNumber.value);
+        let i;
+        let outPut = App.htmlElements.productNumberOutput;
 
-            let tablePrint = [
-            "<table><thead><tr><th>#</th><th>RESULTADO</th><th>ELIMINAR</th></tr></thead><tbody>"
-            ]
+        let tablePrint = [
+          `<table id="tableProduct">
+                  <thead>
+                          <tr>
+                              <th>#</th>
+                              <th>RESULTADO</th>
+                              <th>ELIMINAR</th>
+                          </tr>
+                  </thead>
+            <tbody>`
+        ]
 
-            for (i=0; i <= 10;i++){
-                tablePrint.push(`<${i}tr><td>${product} x ${i+1}</td><td>${product * (i+1)}</td>`);
+        for (i = 0; i <= 10; i++) {
+          tablePrint += `<tr>
+            <td>${product} x ${i + 1}</td>
+                <td>${product * (i + 1)}</td>
+                <td>
+                    <button id="buttonN${i}">Eliminar</button>   
+                </td>
+                </tr>`
 
-                const columnButton = document.createElement("td")
-                const buttonDelete = document.createElement("button")
-
-                columnButton.appendChild(buttonDelete)
-
-                buttonDelete.addEventListener('click', tablePrint[i].remove)
-
-            }
-
-            
-
-            tablePrint.push("</tbody></table>");
-
-            tablePrint.forEach(item => {
-                App.methods.print(outPut, tablePrint.join(''));
-            });
-        },
-
-        print(value, txt){
-            value.innerHTML = txt;
         }
 
-      },
-    };
+        tablePrint += "</tbody></table>"
 
-    App.init();
-  })();
+          App.methods.print(outPut, tablePrint);
+
+      },
+
+      print(value, txt) {
+        value.innerHTML = txt;
+      }
+
+      ,deleteCurrentRow(e){
+      if (e.target.tagName === "BUTTON") {
+    e.target.closest("tr").remove();
+  }
+
+      }
+
+      ,numberValidation(e){
+        e.preventDefault();
+        const number = parseInt(e.target.elements.productNumber.value);
+        const warningOutput = App.htmlElements.productNumberOutput;
+        let text
+        if (number > 20 || number < 1){
+          text = "Ingrese un número entre 1 y 20"
+        App.methods.print(warningOutput,text)}
+        else
+        App.methods.generateProductTable(e);
+      }
+
+    },
+  };
+
+  App.init();
+})();
